1f88581317a8003aca899ea65a7f91aee48a3ff3
Joshua Alphonse <joshalphonse@gmail.com>
2018-12-11 05:13:29 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,19 @@
 require 'pry'
-
 def my_all?(collection)
+  empty_array = []
+  i = 0
+  while i < collection.length
+  empty_array<<
+    yield(collection[i])
+    i = i + 1
+  end
+  
+  if
+  empty_array.include?(false)
+  false
+else
+  true
+end
 
+  
 end
